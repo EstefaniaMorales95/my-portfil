4628dd9458179b311dc265e1d5db16aff51b9d92
Estefa Morales <estefamorales1995@icloud.com>
2024-12-12 18:20:46 -0500

    changep

--- components/counter-services.tsx
+++ components/counter-services.tsx
@@ -1,10 +1,10 @@
 import { dataCounter } from "@/data";
-import CountUp from "react-countup";
+
 
 const CounterServices = () => {
     return (
         <div className="grid justify-between max-w-3xl grid-cols-2 gap-3 mx-auto my-8 md:flex md:grid-cols-4 md:gap-6">
-            {dataCounter.map(({ id, endCounter, text, lineRight, lineRightMobile }) => (
+            {dataCounter.map(({ id, text, lineRight, lineRightMobile }) => (
                 <div key={id} className={`${lineRight && 'ltr'}`}>
                     <div className={`${lineRight && 'px-4 border-2 border-transparent md:border-e-gray-100'} ${lineRightMobile && 'border-e-gray-100'}`} >
                        
